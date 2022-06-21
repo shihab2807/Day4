45185f8d858b688d992071578f8acb65455e77ce
ACER <ACER@DESKTOP-6D1AVH9>
2022-06-21 09:17:21 +0100

    day4 done

---
+++ day5/src/day5/demo1.java
@@ -0,0 +1,25 @@
+package day5;
+
+public class demo1 {
+
+	public static void main(String[] args) {
+		int a=5, b=10;
+		if(a<b)
+		{
+			System.out.println("a is smaller than b");
+			
+		}
+		else if (a==b)
+		{
+			System.out.println("a is equal to b");
+		
+		}
+		else 
+		{
+			System.out.println("a is greater than b");
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
